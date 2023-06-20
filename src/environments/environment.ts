42021dccbe3e978ae953b29daf1d948a9f4650df
Finoana71 <finoanamandresy71@gmail.com>
2023-06-20 19:31:38 +0200

    Deploiement

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8010/api/',
-  // apiUrl: 'https://assignment-api-73d7.onrender.com/api/',
+  // apiUrl: 'http://localhost:8010/api/',
+  apiUrl: 'https://mbds-assignment-51-47-api.onrender.com/api/',
   apiKey: 'token'
 };
